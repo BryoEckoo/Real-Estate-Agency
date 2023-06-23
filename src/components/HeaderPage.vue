50b96659b5263cdcc0c84710fffcf1f0d4dc085b
Brian Rono <chikoe26@gmail.com>
2023-06-23 15:20:18 +0300

    m

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,61 @@
+
+<template>
+    <div class="nav center left vvvv">
+        <img src="../assets/logo.png" alt="logo" />
+        <div>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'HeaderPage'
+}
+
+</script>
+
+<style>
+body{
+    margin: 0px;
+}
+.nav{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    height: 100px;
+    margin: 0;
+    
+}
+.center {
+    text-align: center;
+}
+.left {
+    text-align: left;
+}
+img{
+    width: 110px;
+    height: 90px;
+}
+ul{
+    display: flex;
+    text-align: right;
+    text-decoration: none;
+    list-style: none;   
+}
+li{
+    font-size: 12px;
+    padding: 3px;
+}
+.nav div{
+    margin-left: auto;
+}
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <HeaderPage/>
+    <div>
+        <div>
+            <div class="fdiv">
+                <p>REAL ESTATE AGENCY</p>
+                <div>
+                    <img class="fside" src="../assets/fside.jpg" alt="front view">
+                </div>
+            </div>   
+        </div>
+    </div>
+</template>
+
+<script>
+
+import HeaderPage from '../components/HeaderPage.vue'
+
+export default{
+    name:'HomePage',
+    components:{
+        HeaderPage
+    }
+}
+
+</script>
+
+<style>
+.fdiv{
+    background-color: rgb(50, 158, 201);
+    height:400px;
+    margin-top:95px;
+    margin-right:30px;
+}
+.fdiv p{
+    font-size: 70px;
+    text-align: left;
+    height: 50px;
+    width: 50px;
+    color: white;
+    padding-left: 40px;
+    padding-top: 40px;
+}
+.fside{
+    height: 450px;
+    width: 800px;
+}
+
+</style>
