1062a6f71afb31f0a07053611fccd2e6b984679f
Brian Rono <chikoe26@gmail.com>
2023-06-23 10:58:40 +0300

    m

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from "./components/HomePage.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'HomePage',
+      component: HomePage
     },
     {
       path: '/about',
