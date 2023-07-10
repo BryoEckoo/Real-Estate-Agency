d8a1a8d43f407a36267d5913f41326b1aa6565ba
Brian Rono <chikoe26@gmail.com>
2023-07-10 10:00:18 +0300

    m

--- src/routers.js
+++ src/routers.js
@@ -1,11 +1,13 @@
 import HomPage from './components/HomePage.vue'
 import RentPage from './components/RentPage.vue'
+import BuyPage from './components/BuyPage.vue'
 import { createRouter,createWebHistory } from "vue-router"
 
 
 const routes=[
     {path:'/', component:HomPage},
     {path:'/rent', component:RentPage},
+    {path:'/buy', component:BuyPage},
 ]
 
 const router=createRouter({
